121706eec4833360ec5b85b41d33e97dfd3842c2
paranid5 <dinaraparanid@gmail.com>
2022-09-03 12:31:55 +0300

    convert args

--- src/main/kotlin/com/dinaraparanid/plugins/Routing.kt
+++ src/main/kotlin/com/dinaraparanid/plugins/Routing.kt
@@ -1,13 +1,31 @@
 package com.dinaraparanid.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get {
-            call.respondText("VTMServer")
+        route("/convert_video") {
+            get("{url?}{file_name?}{ext?}") {
+                val url = call.parameters["url"] ?: return@get call.respondText(
+                    "No URL to video provided",
+                    status = HttpStatusCode.BadRequest
+                )
+
+                val fileName = call.parameters["file_name"] ?: return@get call.respondText(
+                    "No output file name provided",
+                    status = HttpStatusCode.BadRequest
+                )
+
+                val ext = call.parameters["ext"] ?: return@get call.respondText(
+                    "No output file extension provided",
+                    status = HttpStatusCode.BadRequest
+                )
+
+                call.respondText("Your URL: $url, output file: $fileName.$ext")
+            }
         }
     }
 }
